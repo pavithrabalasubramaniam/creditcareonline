8b1317cc2a436257a0c756e372bbcb614cbe615c
Pavithra <pavithra.b@payoda.com>
2018-03-31 08:48:38 +0530

    Committing project set up and home page

---
+++ wp-content/themes/creditcare/footer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer-section">
+			<div class="row">
+				<div class="col-md-3">
+					<?php dynamic_sidebar('sidebar-2') ?>
+				</div>
+				<div class="col-md-3">
+					<?php dynamic_sidebar('sidebar-3') ?>
+				</div>
+				<div class="col-md-3">
+					<?php dynamic_sidebar('sidebar-4') ?>
+				</div>
+				<div class="col-md-3">
+					<?php dynamic_sidebar('sidebar-5') ?>
+				</div>				
+			</div>
+			</div>
+
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
+
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the twentysixteen footer text for footer customization.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+<script type='text/javascript' src='<?php bloginfo('template_directory'); ?>/js/custom-script.js'></script><?php wp_footer(); ?>
+</body>
+</html>
